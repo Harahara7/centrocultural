eb2e5c51f677d6575c8c3e1322769013033c7dbe
MegamanJ <jhonsanto@gmail.com>
2019-03-14 00:51:26 -0300

    Melhoria no layout

--- CentroCultural/src/java/DAO/LocatarioDAO.java
+++ CentroCultural/src/java/DAO/LocatarioDAO.java
@@ -8,15 +8,16 @@ package DAO;
 import java.util.ArrayList;
 import java.util.List;
 import modelo.Fisico;
+import modelo.Juridico;
 import modelo.Locatario;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import util.HibernateUtil;
 
 public class LocatarioDAO {
-    
-    public void salvar(Locatario locatario){
-    
+
+    public void salvar(Locatario locatario) {
+
         //inicia uma sessão com conexão com BD
         Session s = HibernateUtil.getSessionFactory().openSession();
         //inicia um Transaction ao banco
@@ -27,11 +28,11 @@ public class LocatarioDAO {
         t.commit();
         //Termine a sessão
         s.close();
-        
+
     }//salvar
-    
-    public void remover(Locatario locatario){
-    
+
+    public void remover(Locatario locatario) {
+
         //inicia uma sessão com conexão com BD
         Session s = HibernateUtil.getSessionFactory().openSession();
         //inicia um Transaction ao banco
@@ -42,11 +43,11 @@ public class LocatarioDAO {
         t.commit();
         //Termine a sessão
         s.close();
-        
+
     }//remover
-    
-    public void atualizar(Locatario locatario){
-    
+
+    public void atualizar(Locatario locatario) {
+
         //inicia uma sessão com conexão com BD
         Session s = HibernateUtil.getSessionFactory().openSession();
         //inicia um Transaction ao banco
@@ -57,20 +58,46 @@ public class LocatarioDAO {
         t.commit();
         //Termine a sessão
         s.close();
-        
+
     }//atualizar
-    
-    public List<Locatario> listarLocatarios(){
-        
+
+    public List<Locatario> listarLocatarios() {
+
         //Query em cima dos Objetos! (Classes)
         String queryObj = "from Locatario";
         List<Locatario> listaLocatario = new ArrayList<>();
-        Session s = HibernateUtil.getSessionFactory().openSession();   
+        Session s = HibernateUtil.getSessionFactory().openSession();
         listaLocatario = s.createQuery(queryObj).list();
         s.close();
-        
+
         return listaLocatario;
-        
+
     }//listarClientes
+
+    public List<Fisico> listarLocatariosFisicos() {
+
+        //Query em cima dos Objetos! (Classes)
+        String queryObj = "from Fisico";
+        List<Fisico> listaLocatarioFisico = new ArrayList<>();
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        listaLocatarioFisico = s.createQuery(queryObj).list();
+        s.close();
+
+        return listaLocatarioFisico;
+
+    }//listarQuantidadedePessoaFisica
     
+    public List<Juridico> listarLocatariosJuridicos(){
+        
+        //Query em cima dos Objetos! (Classes)
+        String queryObj = "from Juridico";
+        List<Juridico> listaLocatarioJuridico = new ArrayList<>();
+        Session s = HibernateUtil.getSessionFactory().openSession();   
+        listaLocatarioJuridico = s.createQuery(queryObj).list();
+        s.close();
+ 
+        return listaLocatarioJuridico;
+               
+    }//listarQuantidadedePessoaJuridica
+
 }//class
